c06d4abe48d2ea2a13e2acdc9be9c4bdd05b56d3
Thuy <tpham045@hotmail.com>
2017-09-25 05:49:27 +0000

    Upgrade es6

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AsylumConnectCatalog from './components/AsylumConnectCatalog';
-import asylumConnectCatalogTheme from './theme/asylumConnectCatalogTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
-/**
- * App entry file
- */
-
-//load our css
-//require('../../web/src/stylesheets/main.scss');
-//require('../../node_modules/react-quill/dist/quill.snow.css');
+import App from './components/App'
 
 const rootElement = document.getElementById('main');
 
-render(
-  <MuiThemeProvider muiTheme={getMuiTheme(asylumConnectCatalogTheme)}>
-    <AsylumConnectCatalog />
-  </MuiThemeProvider>,
-  rootElement);
+render(<App />, document.getElementById('root'));
